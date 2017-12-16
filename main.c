3d74e71be0a8dcb909318a728503f00d1cbeadad
chc1129 <chc1129@gmail.com>
2017-12-16 12:54:18 +0000

    split matrix(jump table) proccessing

--- main.c
+++ main.c
@@ -1,87 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-/* proto type */
-int chgStsTask_00( int arg );
-int chgStsTask_01( int arg );
-int chgStsTask_02( int arg );
-int chgStsTask_03( int arg );
-int chgStsTask_04( int arg );
-
-typedef int ( *CHGSTSTASK_FUNC )( int arg );
-
-/* status */
-enum {
-  STS_INIT,
-  STS_001,
-  STS_002,
-  STS_003,
-  STS_004,
-  STS_END,
-  STS_MAX
-} prgSts;
-
-/* event */
-enum {
-  EVENT_001,
-  EVENT_002,
-  EVENT_MAX
-} eventNum;
-
-/* global status */
-int gSts = STS_INIT;
-/* global event */
-int gEvent = EVENT_001;
-
-/* task function */
-int chgStsTask_00( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-int chgStsTask_01( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-int chgStsTask_02( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-int chgStsTask_03( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-int chgStsTask_04( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-int chgStsTask_05( int arg ) {
-  /* fuction action */
-  printf("Path Through %s.\n", __FUNCTION__);
-  return 0;
-}
-
-
-
-
-/* Function Table */
-static CHGSTSTASK_FUNC chgStsTask_table[ STS_MAX ][ EVENT_MAX ] = {
-  { chgStsTask_00, chgStsTask_01 },
-  { chgStsTask_02, chgStsTask_03 },
-  { chgStsTask_04, chgStsTask_05 }
-};
-
-
-
+#include "matrix_task.h"
 
 int main( void ) {
   int ret = 0;
@@ -98,7 +17,5 @@ int main( void ) {
 
   return 0;
  }
-
-
 
 
