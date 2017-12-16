8b9af3c6835753e4772150739f0c2308b1cea5b8
chc1129 <chc1129@gmail.com>
2017-12-16 12:53:36 +0000

    add matrix_task.c matrix_task.h

---
+++ matrix_task.c
@@ -0,0 +1,41 @@
+#include "matrix_task.h"
+
+/* task function */
+int chgStsTask_00( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+int chgStsTask_01( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+int chgStsTask_02( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+int chgStsTask_03( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+int chgStsTask_04( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+int chgStsTask_05( int arg ) {
+  /* fuction action */
+  printf("Path Through %s.\n", __FUNCTION__);
+  return 0;
+}
+
+
+
---
+++ matrix_task.h
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* status */
+enum {
+  STS_INIT,
+  STS_001,
+  STS_002,
+  STS_003,
+  STS_004,
+  STS_END,
+  STS_MAX
+} prgSts;
+
+/* event */
+enum {
+  EVENT_001,
+  EVENT_002,
+  EVENT_MAX
+} eventNum;
+
+/* global status */
+int gSts;
+/* global event */
+int gEvent;
+
+/* proto type */
+int chgStsTask_00( int arg );
+int chgStsTask_01( int arg );
+int chgStsTask_02( int arg );
+int chgStsTask_03( int arg );
+int chgStsTask_04( int arg );
+int chgStsTask_05( int arg );
+
+typedef int ( *CHGSTSTASK_FUNC )( int arg );
+
+/* Function Table */
+static CHGSTSTASK_FUNC chgStsTask_table[ STS_MAX ][ EVENT_MAX ] = {
+  { chgStsTask_00, chgStsTask_01 },
+  { chgStsTask_02, chgStsTask_03 },
+  { chgStsTask_04, chgStsTask_05 }
+};
+
+
+
